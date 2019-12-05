c359d09465533d0639c18b0280c570b6977e3a6a
Adam Selyem <selyem.adam@gmail.com>
2019-12-05 18:56:52 +0100

    Add solutin for day3_2

--- days/03/main.go
+++ days/03/main.go
@@ -12,19 +12,19 @@ import (
 func main() {
 	fmt.Println("Part 1")
 	util.Start()
-	//result := util.OpenFile("03.txt")
-	//firstWire := strings.Split(result[0], ",")
-	//secondWire := strings.Split(result[1], ",")
-	firstWire := strings.Split("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", ",")
-	secondWire := strings.Split("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", ",")
+	result := util.OpenFile("03.txt")
+	firstWire := strings.Split(result[0], ",")
+	secondWire := strings.Split(result[1], ",")
+	//firstWire := strings.Split("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", ",")
+	//secondWire := strings.Split("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", ",")
 
 	size := 1000
 	var centralX, centralY int = size / 2, size / 2
 
-	cross := make(map[int]int)
 	firstMap := drawCable(centralX, centralY, firstWire)
 	secondMap := drawCable(centralX, centralY, secondWire)
 
+	cross := make(map[int]int)
 	for k, v := range firstMap {
 		secondVals := secondMap[k]
 		if secondVals != nil {
@@ -46,12 +46,33 @@ func main() {
 
 	fmt.Println(distance)
 	util.Elapsed()
+
 	fmt.Println("Part 2")
+	util.Start()
+
+	firstDistance := calculateDistance(cross, centralX, centralY, size, firstWire)
+	secondDistance := calculateDistance(cross, centralX, centralY, size, secondWire)
+
+	shortestDistance := 0
+	for k, v := range cross {
+		key := size*k + v
+		if shortestDistance == 0 {
+			shortestDistance = firstDistance[key] + secondDistance[key]
+		} else {
+			actual := firstDistance[key] + secondDistance[key]
+			if actual < shortestDistance {
+				shortestDistance = actual
+			}
+		}
+	}
+
+	fmt.Println(shortestDistance)
+	util.Elapsed()
 }
 
 func drawCable(centralX, centralY int, wire []string) map[int][]int {
 	var posX, posY int = centralX, centralY
-	fmt.Println(posX, posY)
+
 	result := make(map[int][]int)
 	for _, s := range wire {
 		length, err := strconv.Atoi(s[1:len(s)])
@@ -95,6 +116,54 @@ func setMap(output map[int][]int, x, y int) {
 	}
 
 	output[x] = append(output[x], y)
+}
+
+func calculateDistance(cross map[int]int, centralX, centralY, size int, wire []string) map[int]int {
+	var posX, posY int = centralX, centralY
+
+	result := make(map[int]int)
+	distance := 0
+	for _, s := range wire {
+		length, err := strconv.Atoi(s[1:len(s)])
+		util.LogOnError(err)
+		posX, posY, distance = distanceInOneDireciton(posX, posY, length, size, distance, s[0], cross, result)
+	}
+
+	return result
+}
+
+func distanceInOneDireciton(startX, startY, length, size, distance int, direction byte, cross, result map[int]int) (int, int, int) {
+	i := 1
+	switch direction {
+	case 'U':
+		for ; i <= length; i++ {
+			startY++
+			checkCrossing(result, cross, startX, startY, size, distance, i)
+		}
+	case 'R':
+		for ; i <= length; i++ {
+			startX++
+			checkCrossing(result, cross, startX, startY, size, distance, i)
+		}
+	case 'D':
+		for ; i <= length; i++ {
+			startY--
+			checkCrossing(result, cross, startX, startY, size, distance, i)
+		}
+	case 'L':
+		for ; i <= length; i++ {
+			startX--
+			checkCrossing(result, cross, startX, startY, size, distance, i)
+		}
+	}
+
+	return startX, startY, distance + i - 1 //the last i++ will cause bad distance calculation
+}
+
+func checkCrossing(result, cross map[int]int, x, y, size, distance, i int) {
+	if cross[x] == y {
+		result[x*size+y] = distance + i
+	}
 }
 
 func contains(slice []int, val int) bool {
