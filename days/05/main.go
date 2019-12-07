d9e492c9e37b7e54b23cb778f1534a091a42105a
Adam Selyem <selyem.adam@gmail.com>
2019-12-07 10:43:52 +0100

    Add solution for day05 part 1

---
+++ days/05/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	util "github.com/madimaa/aoc2019/lib"
+)
+
+func main() {
+	fmt.Println("Part 1")
+	util.Start()
+	result := util.OpenFile("05.txt")
+	content := strings.Split(result[0], ",")
+	// result := "1002,4,3,4,33"
+	// content := strings.Split(result, ",")
+	intcode := make([]int, len(content))
+	for i, s := range content {
+		number, err := strconv.Atoi(s)
+		util.LogOnError(err)
+		intcode[i] = number
+	}
+
+	fmt.Println(computer(intcode))
+	util.Elapsed()
+}
+
+func computer(input []int) int {
+	//making a copy of the slice will prevent modifying the `background array`
+	intcode := make([]int, len(input))
+	copy(intcode, input)
+
+	for i := 0; i < len(intcode); {
+		num := intcode[i]
+		opCode := getOpCode(num)
+		//fmt.Println(opCode)
+		if opCode == 99 {
+			break
+		}
+
+		if opCode == 1 || opCode == 2 {
+			//fmt.Println(getDigit(num, 2))
+			//fmt.Println(getDigit(num, 3))
+			noun := getValue(intcode, i+1, getDigit(num, 2))
+			verb := getValue(intcode, i+2, getDigit(num, 3))
+			result := 0
+			if opCode == 1 {
+				result = noun + verb
+			} else {
+				result = noun * verb
+			}
+
+			putValue(intcode, i+3, getDigit(num, 4), result)
+			i += 4
+		} else if opCode == 3 {
+			reader := bufio.NewReader(os.Stdin)
+			text, _, err := reader.ReadLine()
+			util.LogOnError(err)
+			num, err := strconv.Atoi(string(text))
+			util.PanicOnError(err)
+			putValue(intcode, i+1, getDigit(num, 2), num)
+			i += 2
+		} else if opCode == 4 {
+			fmt.Println(getValue(intcode, i+1, getDigit(num, 2)))
+			i += 2
+		}
+	}
+
+	return intcode[0]
+}
+
+func getOpCode(input int) int {
+	if input > 99 {
+		return getDigits(input, 0, 2)
+	}
+
+	return input
+}
+
+func getDigit(input, from int) int {
+	//fmt.Println(input, from)
+	return getDigits(input, from, from+1)
+}
+
+func getDigits(input, from, to int) int {
+	s := strconv.Itoa(input)
+	length := len(s)
+	if (length == from || length < from) && length < to {
+		return 0
+	} else if length >= from && length < to {
+		num, err := strconv.Atoi(s[from:length])
+		util.PanicOnError(err)
+		return num
+	} else {
+		num, err := strconv.Atoi(s[length-to : length-from])
+		util.PanicOnError(err)
+		return num
+	}
+}
+
+func getValue(intcode []int, position, mode int) int {
+	switch mode {
+	case 0:
+		return intcode[intcode[position]]
+	case 1:
+		return intcode[position]
+	default:
+		panic("Something went wrong")
+	}
+}
+
+func putValue(intcode []int, position, mode, value int) {
+	switch mode {
+	case 0:
+		intcode[intcode[position]] = value
+	case 1:
+		intcode[position] = value
+	default:
+		panic("Something went wrong")
+	}
+}
