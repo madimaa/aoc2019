ae0f75bc916c48c68279a6a816332251e733ca86
Adam Selyem <selyem.adam@gmail.com>
2019-12-09 19:15:19 +0100

    Code optimalization
    bruteforce is gone

--- days/06/main.go
+++ days/06/main.go
@@ -12,59 +12,33 @@ func main() {
 	util.Start()
 	result := util.OpenFile("06.txt")
 	//result := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"}
-	tree := make(map[string][]string)
+	backward := make(map[string]string)
+
 	for _, s := range result {
 		split := strings.Split(s, ")")
 		left := split[0]
 		right := split[1]
-		if tree[left] == nil {
-			tree[left] = make([]string, 0)
-		}
 
-		tree[left] = append(tree[left], right)
-
-		for k, v := range tree {
-			if util.ContainsStr(v, left) {
-				tree[k] = append(tree[k], right)
-			}
-		}
-	}
-
-	//okay it is not an algorithm. it is just bruteforce
-	changed := true
-	for changed {
-		changed = addRemainder(tree)
+		backward[right] = left
 	}
 
 	sum := 0
-	for _, v := range tree {
-		sum += len(v)
+	for k := range backward {
+		sum += goBackwards(backward, k)
 	}
 
 	fmt.Println(sum)
 	util.Elapsed()
 }
 
-func addRemainder(tree map[string][]string) bool {
-	ret := false
-	for k, v := range tree {
-		var newValues = make([]string, len(v))
-		copy(newValues, v)
-		for _, x1 := range newValues {
-			if tree[x1] != nil {
-				for _, x2 := range tree[x1] {
-					if !util.ContainsStr(newValues, x2) {
-						ret = true
-						newValues = append(newValues, x2)
-					}
-				}
-			}
-		}
+func goBackwards(backward map[string]string, start string) int {
+	distance := 0
 
-		if ret {
-			tree[k] = newValues
-		}
+	v, ok := backward[start]
+	for ok {
+		distance++
+		v, ok = backward[v]
 	}
 
-	return ret
+	return distance
 }
